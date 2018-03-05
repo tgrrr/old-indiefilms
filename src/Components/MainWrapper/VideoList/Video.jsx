8dc03f06d427130a648c953217b6d21925706d29
botbotdotdot <phil@tgrrr.com>
2018-03-05 17:24:15 +1100

    Updated video size to fit in browser

--- src/Components/MainWrapper/VideoList/Video.jsx
+++ src/Components/MainWrapper/VideoList/Video.jsx
@@ -4,8 +4,8 @@ import YouTube from "react-youtube";
 class Video extends React.Component {
   render() {
     const opts = {
-      height: "390",
-      width: "640",
+      height: "120",
+      width: "500",
       playerVars: {
         // https://developers.google.com/youtube/player_parameters
         autoplay: 1
