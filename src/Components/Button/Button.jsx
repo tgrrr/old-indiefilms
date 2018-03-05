be244989fc9759b66d9c35fe12700e366054600c
grizz27 <gregpsteinke@gmail.com>
2018-03-05 15:05:18 +0800

    button creation

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import  './Button.css';
+
+const Button = () => <div className="Button__wrapper">click here</div>;
+
+export default Button;
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Footer.css';
+import Button from '../Button/Button';
+const Footer = () => (
 
-const Footer = () => <div className="Footer__wrapper">foot is a leg end</div>;
+  <div className="Footer__wrapper">
+    foot is a leg end
+    <Button />
+  </div>
+
+);
 
 export default Footer;
