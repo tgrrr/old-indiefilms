9409e8ff9b0db1f5ddc236a75686347f1da54cd8
tgrrr <phil@tgrrr.com>
2018-05-10 10:34:06 +1000

    change video, update readme

--- README.md
+++ README.md
@@ -1,24 +1,20 @@
+
+
+## How to contribute
+
+- []
+
+## Styleguide
+
+[AirBNB React]
+
 # TODO is in Trello
-- https://trello.com/b/zBZ4bI1W/indiefilm-project-on-codebuddies
+-
 
 https://codesandbox.io/s/zkyr2l50jx
 
-## File Structure:
-
-Home
-  - Header
-  - Nav
-  - Searchbar
-  - VideoList
-    - Videos
-       - Video
-         - Clickable?
-  - Footer
-VideoPage
-  - URL
-
-- React
-  - Router 4
-
 ## Useful links:
-[Youtube API](https://developers.google.com/youtube/iframe_api_reference#Events)
+
+[Youtube API]: https://developers.google.com/youtube/iframe_api_reference#Events
+[AirBNB React]: https://github.com/airbnb/javascript/tree/master/react
+[Trello]: https://trello.com/b/zBZ4bI1W/indiefilm-project-on-codebuddies
--- src/Components/MainWrapper/VideoList/Video.jsx
+++ src/Components/MainWrapper/VideoList/Video.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import YouTube from 'react-youtube';
 
+const youtubePlaceholderId = 'uUT0K3TtKIk';
+
 class Video extends React.Component {
   _onReady (event) {
     // access to player in all event handlers via event.target
@@ -9,8 +11,8 @@ class Video extends React.Component {
 
   render () {
     const opts = {
-      height: '390',
-      width: '640',
+      height: '170',
+      width: '340',
       playerVars: {
         // https://developers.google.com/youtube/player_parameters
         autoplay: 1
@@ -18,7 +20,7 @@ class Video extends React.Component {
     };
 
     return (
-      <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />
+      <YouTube videoId={youtubePlaceholderId} opts={opts} onReady={this._onReady} />
     );
   }
 }
