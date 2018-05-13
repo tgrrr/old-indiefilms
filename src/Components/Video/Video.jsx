022fc5a1a77af79d49726e04747d189e7b316340
tgrrr <phil@tgrrr.com>
2018-05-13 18:43:21 +1000

    Update react 16.3.2, lint, prop-types, removes vunerabilities

--- src/Components/Video/Video.jsx
+++ src/Components/Video/Video.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Player } from 'video-react';
+import PropTypes from 'prop-types';
 import '../../../node_modules/video-react/dist/video-react.css';
 import './Video.css';
 
@@ -8,5 +9,12 @@ const Video = ({ source }) => (
     <source src={source} className="video-react" />
   </Player>
 );
+
+Video.propTypes = {
+  source: PropTypes.string
+};
+Video.defaultProps = {
+  source: 'https://media.w3.org/2010/05/sintel/trailer_hd.mp4'
+};
 
 export default Video;
