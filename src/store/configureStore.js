30b13536229fe10cef62a47cc5230fcfa7636699
manishsaraan <manish021js@gmail.com>
2018-06-06 13:18:17 +0530

    Saga file renamed

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers/reducers';
-import sagas from '../sagas';
+import sagas from '../sagas/sagas';
 
 const configureStore = () => {
   const sagaMiddleWare = createSagaMiddleware();
